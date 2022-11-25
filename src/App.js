e82e27145085d3a5d21431bc4852d230dc22d9e8
Kamarajelango <rajelangovan18@gmail.com>
2022-11-25 15:44:04 +0530

    non-routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import './App.css';
 import Navigation from './Compoents/Navigation';
 import {ThemeProvider} from './Compoents/Context'
-
-
-
-
-
 function App() {
-
   return <>
     <ThemeProvider>     
         <Navigation />  
